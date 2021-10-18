1811d9b3fa73d27b8340cb4b436b32061e31869d
Chamano Ramashia <chamano.sr@gmail.com>
2021-10-18 10:04:06 +0200

    designing the navbar, done!

--- react-mini/src/components/Navbar.jsx
+++ react-mini/src/components/Navbar.jsx
@@ -1,10 +1,79 @@
 import React from 'react'
+import styled from 'styled-components'
+import { Search, ShoppingCartOutlined } from '@mui/icons-material'
+import { Badge } from '@mui/material'
+
+const Container = styled.div`
+    height:60px;
+`
+const Wrapper = styled.div`
+    padding: 10px 20px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+`
+const Left = styled.div`
+    flex:1;
+    display:flex; 
+    align-items: center;
+`
+const Language = styled.div`
+    font-size: 14px;
+    cursor:pointer;
+`
+const SearchContainer = styled.div`
+    border:1px solid lightgray;
+    display:flex; 
+    align-items: center;
+    margin-left: 25px;
+    padding: 5px;
+`
+const Input  = styled.input`
+    border:none;
+`
+
+const Center = styled.div`
+    flex:1;
+    text-align: center;
+`
+const Logo = styled.h1`
+    font-weight: bold;
+`
+const Right = styled.div`
+    flex:1;
+    display:flex; 
+    align-items: center;
+    justify-content: flex-end;
+`
+const MenuItem = styled.div`
+    font-size: 14px;
+    cursor: pointer;
+    margin-left:25px;
+`
 
 const Navbar = () => {
     return (
-        <div>
-            Navbar
-        </div>
+        <Container>
+           <Wrapper>
+               <Left>
+                   <Language>EN</Language>
+                   <SearchContainer>
+                       <Input/>
+                       <Search style={{color:"gray",fontSize:16}}/>
+                   </SearchContainer>
+               </Left>
+               <Center><Logo>Shabah</Logo></Center>
+               <Right>
+                   <MenuItem>Register</MenuItem>
+                   <MenuItem>SIGN IN</MenuItem>
+                   <MenuItem>
+                    <Badge badgeContent={4} color="primary">
+                        <ShoppingCartOutlined/>
+                    </Badge>
+                   </MenuItem>
+                </Right>
+           </Wrapper>
+        </Container>
     )
 }
 
