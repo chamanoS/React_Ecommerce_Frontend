09685f9698c1167e5835bb80ca702f9c90d366e9
Chamano Ramashia <chamano.sr@gmail.com>
2021-10-19 09:53:05 +0200

    creating category items

--- react-mini/src/data.js
+++ react-mini/src/data.js
@@ -21,3 +21,21 @@ export const sliderItems = [
         bg: "f5fg3e",
     },
 ]
+
+export const categories = [
+    {
+      id: 1,
+      img: "https://images.unsplash.com/photo-1628144029346-8a98676311b6?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGFmcmljYW4lMjBnaXJsfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+      title: "SHIRT STYLE!",
+    },
+    {
+      id: 2,
+      img: "https://images.unsplash.com/photo-1529123202249-4f6224196c9b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8YWZyaWNhbiUyMGJlYXV0eXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+      title: "LOUNGEWEAR LOVE",
+    },
+    {
+      id: 3,
+      img: "https://images.unsplash.com/photo-1536766768598-e09213fdcf22?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTAwfHxhZnJpY2FuJTIwYmVhdXR5fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+      title: "LIGHT JACKETS",
+    },
+  ];
