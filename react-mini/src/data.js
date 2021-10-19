57206e93d5126102e5c3d0148e6073f82fd3be98
Chamano Ramashia <chamano.sr@gmail.com>
2021-10-19 09:15:41 +0200

    adding the slider

---
+++ react-mini/src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+    {
+        id:1,
+        img: "https://images.unsplash.com/photo-1589156191108-c762ff4b96ab?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGJlYXV0aWZ1bCUyMGJsYWNrJTIwbGFkeXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+        title: "Summer Sale",
+        desc: "DONT COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS",
+        bg: "f5fafd",
+    },
+    {
+        id:2,
+        img: "https://images.unsplash.com/photo-1589156191108-c762ff4b96ab?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGJlYXV0aWZ1bCUyMGJsYWNrJTIwbGFkeXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+        title: "Winter Sale",
+        desc: "DONT COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS",
+        bg: "fcf1ed",
+    },
+    {
+        id:3,
+        img: "https://images.unsplash.com/photo-1589156191108-c762ff4b96ab?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGJlYXV0aWZ1bCUyMGJsYWNrJTIwbGFkeXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+        title: "Summer Sale",
+        desc: "DONT COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS",
+        bg: "f5fg3e",
+    },
+]
