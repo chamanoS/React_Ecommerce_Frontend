e999a79a77b8db9148f7fb5ea7abd364eaab20d8
Chamano Ramashia <chamano.sr@gmail.com>
2021-11-01 11:41:34 +0200

    creating register ui

--- react-mini/src/App.js
+++ react-mini/src/App.js
@@ -1,11 +1,13 @@
+import Product from "./pages/Product";
 import Home from "./pages/Home";
 import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
 
 
 function App() {
   return (
     <div >
-      <ProductList/>
+      <Register/>
     </div>
   );
 }
---
+++ react-mini/src/pages/Login.jsx
@@ -0,0 +1,15 @@
+import { styled } from '@mui/system'
+
+const Container = styled.div`
+
+`
+
+const Login = () => {
+    return (
+        <Container>
+            
+        </Container>
+    )
+}
+
+export default Login
