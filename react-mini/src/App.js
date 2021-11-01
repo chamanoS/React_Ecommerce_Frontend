1d773b95c262224c1862885a603ab90680192275
Chamano Ramashia <chamano.sr@gmail.com>
2021-11-01 12:46:06 +0200

    finished cart ui

--- react-mini/src/App.js
+++ react-mini/src/App.js
@@ -3,12 +3,13 @@ import Home from "./pages/Home";
 import ProductList from "./pages/ProductList";
 import Register from "./pages/Register";
 import Login  from "./pages/Login";
+import Cart from "./pages/Cart";
 
 
 function App() {
   return (
     <div >
-      <Login/>
+      <Cart/>
     </div>
   );
 }
